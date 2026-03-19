2092dfac0aad78aa8291cc70869b08b55a73bf07
marcelduplessis <marceldpl10@gmail.com>
2026-03-19 09:34:34 +0100

    fix: nav collapse

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -10,96 +10,29 @@ var $btn = $('#site-nav button');
 var $vlinks = $('#site-nav .visible-links');
 var $hlinks = $('#site-nav .hidden-links');
 
-var breaks = [];
-
 function updateNav() {
-
-  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-  /* original code to move items one by one between lists 
-
-  // The visible list is overflowing the nav
-  if($vlinks.width() > availableSpace) {
-
-    // Record the width of the list
-    breaks.push($vlinks.width());
-
-    // Move item to the hidden list
-    $vlinks.children('*:not(.masthead__menu-item--lg)').last().prependTo($hlinks);
-
-    // Show the dropdown btn
-    if($btn.hasClass('hidden')) {
-      $btn.removeClass('hidden');
-    }
-
-  // The visible list is not overflowing
-  } else {
-
-    // There is space for another item in the nav
-    if(availableSpace > breaks[breaks.length-1]) {
-
-      // Move the item to the visible list
-      $hlinks.children().first().appendTo($vlinks);
-      breaks.pop();
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if(breaks.length < 1) {
-      $btn.addClass('hidden');
-      $hlinks.addClass('hidden');
-    }
-  } original code ends here /*/ 
-
-  // New code to move all overflowing items at once
-  // The visible list is overflowing the nav
-  if ($vlinks.width() > availableSpace) {
-
-    // Move ALL items (except any .masthead__menu-item--lg) to the hidden list
-    $vlinks.children('*:not(.masthead__menu-item--lg)').appendTo($hlinks);
-
-    // Record that we've overflowed; use 1 “break” to indicate all are hidden
-    breaks = [$hlinks.children().length];
-
-    // Show the dropdown button
+  var collapseWidth = 800; // Set your desired breakpoint
+  var menuItemsArray = $vlinks.children('li').toArray();
+  console.log('Before collapse:', menuItemsArray.length, menuItemsArray.map(function(item){return $(item).text();}));
+  if ($(window).width() < collapseWidth) {
+    $hlinks.empty(); // Clear hidden-links before moving
+    menuItemsArray.forEach(function(item){ $(item).appendTo($hlinks); });
     $btn.removeClass('hidden');
-
+    $hlinks.removeClass('hidden');
+    $nav.addClass('collapsed');
+    $('.masthead__menu').addClass('collapsed');
+    $btn.attr('count', $hlinks.children().length);
+    console.log('After collapse:', $hlinks.children().length, $hlinks.children().map(function(){return $(this).text();}).get());
   } else {
-
-    // There is space for another item in the nav AND we are in "all visible" mode
-    if (breaks.length < 1 && $hlinks.children().length === 0) {
-      // normal case: nothing to move back, everything already visible
-    }
-
-    // If you want to allow “all back to visible” when there is enough space:
-    if (breaks.length >= 1 && availableSpace > $nav.width()) {
-      // Move everything back to visible list
-      $hlinks.children().appendTo($vlinks);
-      breaks = [];
-      $btn.addClass('hidden');
-      $hlinks.addClass('hidden');
-    }
-
-    // Hide the dropdown btn if hidden list is empty
-    if ($hlinks.children().length === 0) {
-      $btn.addClass('hidden');
-      $hlinks.addClass('hidden');
-      breaks = [];
-    }
+    var hiddenItemsArray = $hlinks.children('li').toArray();
+    hiddenItemsArray.forEach(function(item){ $(item).appendTo($vlinks); });
+    $btn.addClass('hidden');
+    $hlinks.addClass('hidden');
+    $nav.removeClass('collapsed');
+    $('.masthead__menu').removeClass('collapsed');
+    $btn.attr('count', 0);
   }
-
-
-  // Keep counter updated
-  // $btn.attr("count", breaks.length); original line
-  $btn.attr("count", $hlinks.children().length); // new line to show number of hidden items
-
-  // Recur if the visible list is still overflowing the nav - we have removed the recursive call
-  // if($vlinks.width() > availableSpace) {
-  //   updateNav();
-  // }
-
 }
-
-// Window listeners
 
 $(window).resize(function() {
   updateNav();
