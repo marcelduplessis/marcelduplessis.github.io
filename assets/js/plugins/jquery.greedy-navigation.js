a7f8f855bbbc7e32458a84e1702e243ca3495a20
marcelduplessis <marceldpl10@gmail.com>
2026-03-19 11:35:12 +0100

    fix: masthead

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -5,42 +5,35 @@
 *
 */
 
-var $nav = $('#site-nav');
-var $btn = $('#site-nav button');
-var $vlinks = $('#site-nav .visible-links');
-var $hlinks = $('#site-nav .hidden-links');
-
-function updateNav() {
-  var collapseWidth = 800; // Set your desired breakpoint
-  var menuItemsArray = $vlinks.children('li').toArray();
-  console.log('Before collapse:', menuItemsArray.length, menuItemsArray.map(function(item){return $(item).text();}));
-  if ($(window).width() < collapseWidth) {
-    $hlinks.empty(); // Clear hidden-links before moving
-    menuItemsArray.forEach(function(item){ $(item).appendTo($hlinks); });
-    $btn.removeClass('hidden');
-    $hlinks.removeClass('hidden');
-    $nav.addClass('collapsed');
-    $('.masthead__menu').addClass('collapsed');
-    $btn.attr('count', $hlinks.children().length);
-    console.log('After collapse:', $hlinks.children().length, $hlinks.children().map(function(){return $(this).text();}).get());
-  } else {
-    var hiddenItemsArray = $hlinks.children('li').toArray();
-    hiddenItemsArray.forEach(function(item){ $(item).appendTo($vlinks); });
-    $btn.addClass('hidden');
-    $hlinks.addClass('hidden');
-    $nav.removeClass('collapsed');
-    $('.masthead__menu').removeClass('collapsed');
-    $btn.attr('count', 0);
+$(document).ready(function(){
+  function updateNav() {
+    var collapseWidth = 1066;
+    var $vlinks = $('#site-nav .visible-links');
+    var $hlinks = $('#site-nav .hidden-links');
+    if ($(window).width() < collapseWidth) {
+      $hlinks.empty();
+      $($vlinks.children('li').toArray()).each(function(){ $hlinks.append(this); });
+      $('#site-nav button').removeClass('hidden');
+      $hlinks.removeClass('hidden');
+      $('#site-nav').addClass('collapsed');
+      $('.masthead__menu').addClass('collapsed');
+    } else {
+      $($hlinks.children('li').toArray()).each(function(){ $vlinks.append(this); });
+      $('#site-nav button').addClass('hidden');
+      $hlinks.addClass('hidden');
+      $('#site-nav').removeClass('collapsed');
+      $('.masthead__menu').removeClass('collapsed');
+    }
   }
-}
 
-$(window).resize(function() {
+  $(window).resize(function() {
+    updateNav();
+  });
+
+  $('#site-nav button').on('click', function() {
+    $('#site-nav .hidden-links').toggleClass('hidden');
+    $(this).toggleClass('close');
+  });
+
   updateNav();
 });
-
-$btn.on('click', function() {
-  $hlinks.toggleClass('hidden');
-  $(this).toggleClass('close');
-});
-
-updateNav();
